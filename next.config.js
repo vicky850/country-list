688efc78223179e0ded699788b1a5413c7411012
vicky850 <40638484+vicky850@users.noreply.github.com>
2024-10-08 14:32:10 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "flagcdn.com",
-      "upload.wikimedia.org",
-      "lh3.googleusercontent.com",
-      "avatars.githubusercontent.com",
-      "githubusercontent.com",
-      "pbs.twimg.com",
-      "media.licdn.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
     ],
   },
 };
